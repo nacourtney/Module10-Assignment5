31785a535874fb78d224156cb41236ac3ebfd542
Nichole Courtney <nichole.a.courtney@gmail.com>
2022-11-20 10:09:11 -0700

    Initial Commit: Notification Working

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  SafeAreaView,
+} from "react-native";
+import Notification from "./Notification";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    notify: false,
+    message: "This is a notification!",
+  };
+
+  toggleNotification = () => {
+    this.setState({
+      notify: !this.state.notify,
+    });
+  };
+
+  render() {
+    const notify = this.state.notify ? (
+      <Notification
+        autoHide
+        message={this.state.message}
+        onClose={this.toggleNotification}
+      />
+    ) : null;
+    return (
+      <SafeAreaView>
+        <Text style={styles.toolbar}>Main toolbar</Text>
+        <View style={styles.content}>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna.
+          </Text>
+          <TouchableOpacity
+            onPress={this.toggleNotification}
+            style={styles.btn}
+          >
+            <Text style={styles.text}>Show notification</Text>
+          </TouchableOpacity>
+          <Text>
+            Sed ut perspiciatis unde omnis iste natus error sit accusantium
+            doloremque laudantium.
+          </Text>
+          {notify}
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  toolbar: {
+    backgroundColor: "#8e44ad",
+    color: "#fff",
+    fontSize: 22,
+    padding: 20,
+    textAlign: "center",
+  },
+  content: {
+    padding: 10,
+    overflow: "hidden",
+  },
+  btn: {
+    margin: 10,
+    backgroundColor: "#9b59b6",
+    borderRadius: 3,
+    padding: 10,
+  },
+  text: {
+    textAlign: "center",
+    color: "#fff",
   },
 });
